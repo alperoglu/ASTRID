22db996b20684dfcf0ae5af58db402905a6681e5
Alper Eroğlu <alper.eroglu.ae@gmail.com>
2024-09-06 17:50:41 +0200

    Update RunASTRiD_Ji.sh

--- RunASTRiD_Ji.sh
+++ RunASTRiD_Ji.sh
@@ -11,12 +11,12 @@ else
 fi
 
 # Define the paths for the input and output files
-ADATA_FILE="/srv/mfs/hausserlab/data/GRINT/Ji2020SCC/sc/${SAMPLE_TO_TEST}_preRefSTRA.h5ad"
+ADATA_FILE="/path/to/file/${SAMPLE_TO_TEST}_preASTRID.h5ad"
 PREFIX=$SAMPLE_TO_TEST
-OUTPUT_FILE="/srv/mfs/hausserlab/data/GRINT/Ji2020SCC/sc/${SAMPLE_TO_TEST}_v0.03_outRefSTRA.h5ad"
-CLUSTERING_RESULT="/scratch/alper.eroglu/GRINT/new_outputs/${SAMPLE_TO_TEST}/${SAMPLE_TO_TEST}_RefSTRA_v0.03_Result.csv"
+OUTPUT_FILE="/path/to/file/${SAMPLE_TO_TEST}_v0.03_outASTRID.h5ad"
+CLUSTERING_RESULT="/path/to/file/${SAMPLE_TO_TEST}/${SAMPLE_TO_TEST}_ASTRID_v0.01_Result.csv"
 AUTHOR_TYPE="level2_celltype"
-LOG_FILE="/scratch/alper.eroglu/GRINT/new_outputs/${SAMPLE_TO_TEST}/${SAMPLE_TO_TEST}_RefSTRA_v0.03.log"
+LOG_FILE="/path/to/file/${SAMPLE_TO_TEST}/${SAMPLE_TO_TEST}_ASTRID_v0.01.log"
 
 # Print the paths to check them
 echo "Adata file: $ADATA_FILE"
@@ -45,27 +45,26 @@ fi
 
 # if the user wants to run all, run the script with the --all option
 if [ $RUN_TYPE == "f" ]; then
-    echo "Running full RefSTRA with the --all option..."
-    nice -19 python3 /scratch/alper.eroglu/GRINT/RefSTRA_v0.03.py --all --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
-    # { time nice -19 python3 /scratch/alper.eroglu/GRINT/RefSTRA_v0.03.py --all --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE ; } 2>> $LOG_FILE
+    echo "Running full ASTRID with the --all option..."
+    nice -19 python3 /path/to/script/folder/ASTRID_v0.01.py --all --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
 
 fi
 
 if [ $RUN_TYPE == "a" ]; then
-    echo "Running RefSTRA with the --annotation option..."
-    nice -19 python3 /scratch/alper.eroglu/GRINT/RefSTRA_v0.03.py --annotation  --validation --damage --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
+    echo "Running ASTRID with the --annotation option..."
+    nice -19 python3 /path/to/script/folder/ASTRID_v0.01.py --annotation  --validation --damage --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
 fi
 
 # if the user wants to run the validation, run the script with the --validation option
 if [ $RUN_TYPE == "v" ]; then
-    echo "Running RefSTRA with the --validation option..."
-    nice -19 python3 /scratch/alper.eroglu/GRINT/RefSTRA_v0.03.py --validation --damage --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
+    echo "Running ASTRID with the --validation option..."
+    nice -19 python3 /path/to/script/folder/ASTRID_v0.01.py --validation --damage --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
 fi
 
 # if the user wants to run the damage, run the script with the --damage option
 if [ $RUN_TYPE == "d" ]; then
-    echo "Running RefSTRA with the --damage option..."
-    nice -19 python3 /scratch/alper.eroglu/GRINT/RefSTRA_v0.03.py --damage --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
+    echo "Running ASTRID with the --damage option..."
+    nice -19 python3 /path/to/script/folder/ASTRID_v0.01.py --damage --input_file $ADATA_FILE --input_prefix $PREFIX --output_file $OUTPUT_FILE --output_clustering_results $CLUSTERING_RESULT --author_type $AUTHOR_TYPE > $LOG_FILE 2>&1
 fi
 
-echo "Done running RefSTRA! Check the log file " $LOG_FILE " for more details."
+echo "Done running ASTRID! Check the log file " $LOG_FILE " for more details."
